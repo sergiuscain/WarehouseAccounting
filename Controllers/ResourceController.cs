0c250da3121624c5fee17e447f34b71e74756258
Sergey Korolev <sergiussmithe@gmail.com>
2025-08-03 12:50:20 +0300

    Удалил начальный контроллер с погодой. Добавил контролле с двумя Get методами для ресурсов. И модель ресурсов.

---
+++ Controllers/ResourceController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WarehouseAccounting.Models;
+
+namespace WarehouseAccounting.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ResourceController : ControllerBase
+    {
+        private List<Resource> resources = new List<Resource>
+        {
+           new Resource { Id = 0, IsActive = false, Name = "Null"},
+           new Resource { Id = 1, IsActive = true, Name = "ABS филамент"},
+           new Resource { Id = 2, IsActive = true, Name = "Хотенд"},
+           new Resource { Id = 3, IsActive = true, Name = "PLAA филамент"}
+
+        };
+
+        [HttpGet("GetAll")]
+        public async Task<ActionResult<List<Resource>>> GetAll()
+        {
+            return resources;
+        }
+
+        [HttpGet("GetById")]
+        public ActionResult<Resource> GetById(int id)
+        {
+            var result = resources.FirstOrDefault(x => x.Id == id);
+            return result == null ? resources.First() : result;
+        }
+    }
+}
---
+++ Models/Resource.cs
@@ -0,0 +1,9 @@
+﻿namespace WarehouseAccounting.Models
+{
+    public class Resource
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
