9aaf9607e856bd22d23993611f7d3eaee0ab2fac
Sergey Korolev <sergiussmithe@gmail.com>
2025-08-05 13:49:12 +0300

    Исправил ошибку в модели ресурса поступления.

--- Models/ResourceOfAdmission.cs
+++ Models/ResourceOfAdmission.cs
@@ -1,12 +1,12 @@
 ﻿namespace WarehouseAccounting.Models
 {
-    //Ресурс поступления (идентификатор, идентификатор ресурса, идентификатор единицы измерения, количество)
+    //Ресурс поступления (идентификатор, идентификатор документа поступления, идентификатор ресурса, идентификатор единицы измерения, количество)
     public class ResourceOfAdmission
     {
         public Guid Id { get; set; }
+        public AdmissionDocument AdmissionDocument { get; set; }
         public Resource ResourceId { get; set; }
         public UnitOfMeasurement UnitOfMeasurement { get; set; }
         public int Count { get; set; }
-        public Guid AdmissionDocumentId { get; set; }
     }
 }
