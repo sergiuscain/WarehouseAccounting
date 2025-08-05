818885b7980013bb1545fa16208f6ff8e2e725ce
Sergey Korolev <sergiussmithe@gmail.com>
2025-08-05 14:40:27 +0300

    Добавил список ресурсов поступления в модель документа поступления для более удобного доступа к ресурсам поступления.

--- Models/AdmissionDocument.cs
+++ Models/AdmissionDocument.cs
@@ -6,5 +6,8 @@
         public Guid Id { get; set; }
         public int Number { get; set; }
         public DateTime Date { get; set; }
+
+        // Связь с ресурсами поступления. Один документ поступления ко многим ресурсам поступления.
+        public List<ResourceOfAdmission> ResourceOfAdmissions { get; set; }
     }
 }
