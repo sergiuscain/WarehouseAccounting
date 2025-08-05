2c0a9d87ddadb9bc36ed1f1d6f7bf039c3353de7
Sergey Korolev <sergiussmithe@gmail.com>
2025-08-05 14:07:50 +0300

    Добавил класс-сервис для работы с ресурсами поступления. (Пока что методы для создания и удаления только). И интерфейс для него.

---
+++ Interfaces/IResourceOfAdmissionsStorage.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WarehouseAccounting.Models;
+
+namespace WarehouseAccounting.Interfaces
+{
+    public interface IResourceOfAdmissionsStorage
+    {
+        Task<ActionResult<bool>> Create(ResourceOfAdmission resourceOfAdmission);
+        Task<bool> Delete(Guid id);
+    }
+}
---
+++ Services/ResourceOfAdmissionsStorage.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WarehouseAccounting.DB;
+using WarehouseAccounting.Interfaces;
+using WarehouseAccounting.Models;
+
+namespace WarehouseAccounting.Services
+{
+    public class ResourceOfAdmissionsStorage : IResourceOfAdmissionsStorage
+    {
+        MyDbContext _context;
+        public ResourceOfAdmissionsStorage(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("Create")]
+        public async Task<ActionResult<bool>> Create(ResourceOfAdmission resourceOfAdmission)
+        {
+            //Нельзя создать ресурс поступления с таким же Id, который уже существует. 
+            if (_context.ResourceOfAdmissions.FirstOrDefault(ra => ra.Id == resourceOfAdmission.Id) == null)
+            {
+                return false;
+            }
+            _context.ResourceOfAdmissions.Add(resourceOfAdmission);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+        [HttpDelete("Delete")]
+        public async Task<bool> Delete(Guid id)
+        {
+            var resourceOfAdmissions = await _context.ResourceOfAdmissions.FirstOrDefaultAsync(ra => ra.Id == id);
+            //Если ресурс поступления не найден, возвращаем false.
+            if (resourceOfAdmissions == null)
+            {
+                return false;
+            }
+            await _context.ResourceOfAdmissions.AddAsync(resourceOfAdmissions);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
